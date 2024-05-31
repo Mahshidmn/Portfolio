d26f570e7b68c5752d1499c7fe0a3a2adf8cbf55
mahshidmn <ma.mojtabavi@gmail.com>
2024-05-31 18:57:01 +1000

    contact section responsivness eddited

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -11,7 +11,7 @@ const Footer = () => {
         <div className='pt-[4rem] md:pt-[8rem] pb-[5rem] bg-[#02050a]'>
             <h1 className='font-bold text-white text-[50px] mb-[3rem] mx-auto text-center'>Contact</h1>
             {/* <div className='grid border-b-[1px] pb-[6rm] border-gray-400 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mx-auto gap-[3rem]'> */}
-            <div className='flex flex-col pb-[3rem] border-gray-400  ml-[4rem] md:ml-[8rem]'>
+            <div className='flex flex-col pb-[3rem] border-gray-400  ml-[2rem] md:ml-[6rem]'>
                 <div className='flex items-center justify-start space-x-6 py-[2rem] '>
                     <div className='md:w-[4rem] md:h-[4rem] w-[3rem] h-[3rem] flex items-center justify-center rounded-full bg-yellow-400'>
                         <EnvelopeIcon className='md:w-[2.5rem] md:h-[2.5rem] w-[2rem] h-[2rem] text-black' />
@@ -61,7 +61,7 @@ const Footer = () => {
                     <div className='text-[16px] mb-[2rem] md:mb-0 text-white opacity-50'>
                             Mahshid M.Naeini 2024 | all Rights Reserved
                     </div>
-                    <div className='items-center space-x-10 text-white pl-[7rem] grid grid-cols-1 md:grid-cols-2 '>
+                    <div className='items-center space-x-10 text-white pl-[10rem] grid grid-cols-1 md:grid-cols-2 '>
                             <p className='text-[16px] text-white opacity-50'>Terms & Condition</p>
                             <p className='text-[16px] text-white opacity-50'>Privacy Policy</p>
                             <p className='text-[16px] text-white opacity-50'>Sitemap</p>
