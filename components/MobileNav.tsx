55385921606d20237b1f01c7f95320c1429a1344
mahshidmn <ma.mojtabavi@gmail.com>
2024-06-01 17:30:13 +1000

    Mobile Nav updated

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -7,24 +7,31 @@ interface Props {
 }
 
 const MobileNav = ({ nav, closeNav }: Props) => {
-const navAnimation = nav ? "translate-x-0" : "translate-x-[-100%]";
+    const navAnimation = nav ? "translate-x-0" : "translate-x-[-100%]";
 
-  return (
-    <div className={`fixed ${navAnimation} transform transition-all duration-300 top-0 left-0 right-0 bottom-0 z-[1000000] bg-[#09101a]`}>
+    const handleNavClick = (sectionId: string) => {
+        const section = document.getElementById(sectionId);
+        if (section) {
+            section.scrollIntoView({ behavior: 'smooth' });
+            closeNav();
+        }
+    };
 
-        <div className='w-[100vw] h-[100vh] flex flex-col items-center justify-center'>
-            <div className='nav-link-mobile'>HOME</div>
-            <div className='nav-link-mobile'>ABOUT</div>
-            <div className='nav-link-mobile'>SERVICES</div>
-            <div className='nav-link-mobile'>SKILLS</div>
-            <div className='nav-link-mobile'>PROJECTS</div>
-            <div className='nav-link-mobile'>CONTACT</div>
+    return (
+        <div className={`fixed ${navAnimation} transform transition-all duration-300 top-0 left-0 right-0 bottom-0 z-[1000000] bg-[#09101a]`}>
+            <div className='w-[100vw] h-[100vh] flex flex-col items-center justify-center'>
+                <div className='nav-link-mobile' onClick={() => handleNavClick('home')}>HOME</div>
+                <div className='nav-link-mobile' onClick={() => handleNavClick('about')}>ABOUT</div>
+                <div className='nav-link-mobile' onClick={() => handleNavClick('services')}>SERVICES</div>
+                <div className='nav-link-mobile' onClick={() => handleNavClick('skills')}>SKILLS</div>
+                <div className='nav-link-mobile' onClick={() => handleNavClick('projects')}>PROJECTS</div>
+                <div className='nav-link-mobile' onClick={() => handleNavClick('footer')}>CONTACT</div>
+            </div>
+            <div onClick={closeNav} className='absolute z-[100000000] cursor-pointer top-[2rem] right-[2rem] w-[2rem] h-[2rem] text-yellow-400'>
+                <XMarkIcon />
+            </div>
         </div>
-        <div onClick={ closeNav } className='absolute z-[100000000] cursor-pointer top-[2rem] right-[2rem] w-[2rem] h-[2rem] text-yellow-400'>
-            <XMarkIcon />
-        </div>
-    </div>
-  );
+    );
 };
 
 export default MobileNav;
